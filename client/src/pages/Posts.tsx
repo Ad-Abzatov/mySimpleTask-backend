f4cf0f636b3072ae72739457ce66e2d3cfeff647
seyar_ad <ad-abzatov.seyar@mail.ru>
2025-12-04 23:21:29 +0300

    second commit

---
+++ client/src/pages/Posts.tsx
@@ -0,0 +1,88 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import AuthButton from "../components/LogoutButton";
+import { jwtDecode } from "jwt-decode";
+
+interface toDo {
+  id: number
+  title: string
+}
+
+interface IdField {
+  id: number | string
+}
+
+const getUserId = () => {
+  let decodedToken;
+  const token = localStorage.getItem('Bearer');
+  if (typeof token === 'string') {
+    decodedToken = jwtDecode(token);
+  }
+  const {id} = decodedToken as IdField
+  return id
+}
+
+const Posts = () => {
+  const [posts, setPosts] = useState<toDo[]>([]);
+  const [title, setTitle] = useState('');
+
+  const fetchPosts = async () => {
+    const userId = getUserId();
+    const response = await axios.get(`http://localhost:5000/api/post/userposts/${userId}`);
+    setPosts(response.data);
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const addPost = async (e: any) => {
+    e.preventDefault();
+    const newPost = {title};
+    await axios.post('http://localhost:5000/api/post/posts', newPost);
+    setTitle('');
+    fetchPosts();
+  }
+
+  const delPost = async (id: any) => {
+    await axios.delete(`http://localhost:5000/api/post/posts/${id}`);
+    fetchPosts();
+  }
+
+  const updPost = async (id: any, data: string) => {
+    const newData = window.prompt(data, data);
+    if (newData !== null) {
+      await axios.put(`http://localhost:5000/api/post/posts/${id}`, {title: newData});
+      fetchPosts();
+    }
+  }
+
+  return (
+    <div className='Posts'>
+      <AuthButton />
+      <form onSubmit={addPost}>
+        <div className="AddTask">
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+          <button type="submit">Добавить задачу</button>
+        </div>
+      </form>
+        <h1>Список задач</h1>
+        {posts.map((post) => (
+          <div>
+            <p>
+              {post.title}
+              <button onClick={() => updPost(post.id, post.title)}>Изменить</button>
+              <button onClick={() => delPost(post.id)}>Удалить</button>
+            </p>
+          </div>
+        ))}
+    </div>
+  )
+}
+
+export default Posts
